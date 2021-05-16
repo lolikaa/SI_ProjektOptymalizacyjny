0278ebe4f096551544e895d5ebf13128cbdb3eb2
id github: lolikaa <monik.gabrys@wp.pl>
2021-05-16 11:18:51 +0200

    next algorithm step added
    we are looking for a new single solution in the loop (in two ways, depending on the randomized variable r1. Then we check if the new solution is better than the random one generated in the previous step.

---
+++ algNextStep.js
@@ -0,0 +1,88 @@
+const algorytm = require('./algorytm.js');
+const faker = require('faker');
+const fs = require('fs');
+const parse = require('csv-parse');
+const { FORMERR } = require('dns');
+
+const algNextStep = () => {
+
+    const maxIteracji = 1000;
+    const HMCR = 70; //prawdopodobieństwo wystąpienia w %
+   
+    // console.log("długośc HM: " + HM.length);
+    // console.log("długośc FP: " + FP.length);
+
+    fs.createReadStream(__dirname + '/data.csv')
+        .pipe(parse({ columns: true }, function (err, results) {
+
+            for (i = 0; i < maxIteracji; i++) {
+                let tmpHM = []; // random items => sum <= maxWeight
+                let tmpFP = []; //[total usefulness]
+                let totalWeight = 0;
+                let totalUsefulness =0;
+                let iRandom;
+
+                //generowanie losowej liczby r1 od 1 do 100
+                let r1 = faker.datatype.number({
+                    'min': 1,
+                    'max': 100
+                });
+
+
+                if (r1 < HMCR) {
+                    //szukam nowego rozwiązania z pamięci algorytmu HM
+                    for (j = 0; j < HM.length; j++) {
+                        do {
+                                iRandom = faker.datatype.number({
+                                'min': 0,
+                                'max': HM.length-1
+                            });
+                        } while ((HM[j][iRandom] === undefined));
+
+                        let iWeight = parseFloat(results[iRandom]['WEIGHT [g]']);
+                        let iUsefulness = parseInt(results[iRandom]['USEFULNESS [1-100]']);
+
+                        // checking the weight of items
+                        totalWeight = totalWeight + iWeight; //totalWeight = totalWeight + random itemWeigth from column [i]; 
+                        if (totalWeight <= maxWeight) {
+                            tmpHM.push(iRandom);
+                            totalUsefulness = totalUsefulness + iUsefulness;
+                        }
+                    }
+                    tmpFP.push(totalUsefulness);
+                }
+                else {
+                    // szukam rozwiązania w całej bazie tak samo jak w 1 części algorytmu
+                    for (j=0; totalWeight < maxWeight; j++) {
+        
+                            iRandom = faker.datatype.number({
+                            'min': 0,
+                            'max': results.length-1});
+    
+                        let iWeight = parseFloat(results[iRandom]['WEIGHT [g]']);
+                        let iUsefulness = parseInt(results[iRandom]['USEFULNESS [1-100]']);
+    
+                        // checking the weight of items
+                        totalWeight = totalWeight + iWeight;
+                        if (totalWeight <= maxWeight) {
+                            tmpHM.push(iRandom);
+                            totalUsefulness = totalUsefulness + iUsefulness;
+                        } 
+                    }
+                    tmpFP.push(totalUsefulness);
+                }
+                // TODO
+                // po każdej iteracji porównujemy nowe rozwiązanie z istniejącymi
+                for (k=0; k<FP.length; k++) {
+                    if(tmpFP[0] > FP[k]) {
+                        FP[k] = tmpFP;
+                        HM[k] = tmpHM;
+                        break;
+                    };
+                }
+                
+            }
+        }));
+}
+
+module.exports = algNextStep;
--- algorytm.js
+++ algorytm.js
@@ -1,53 +1,43 @@
 const faker = require('faker');
 const fs = require('fs');
 const parse = require('csv-parse');
+const algNextStep = require('./algNextStep');
 
 const algorytm = () => { 
 
-let HM =[];
-let FP=[]; //[total weight, total usefulness]
-const HMS=15;
-const maxWeight = 10000; //10 kg w gramach
-
-let parser = parse({columns: true}, function (err, results){
-    //       console.log(results);
- 
-    for(i=0; i<HMS; i++) {
-        let arrHM = [];
-        let itemWeigth=0;
-        let totalWeight =0;
-        let totalUsefulness=0;
-  
-            for(j=0; totalWeight<maxWeight; j++){
+fs.createReadStream(__dirname+'/data.csv')
+.pipe(parse({columns: true}, function (err, results) {
+          // console.log(results);
+        for (i=0; i < HMS; i++) {
+            let tmpHM = [];
+            let totalWeight =0;
+            let totalUsefulness=0;
     
-                let random = faker.datatype.number({
-                    'min': 0,
-                    'max': results.length-1});
-                // console.log( "element j = " + j + " to id tablicy rezultatów : " + random);
+                for (j=0; totalWeight < maxWeight; j++) {
+        
+                    let iRandom = faker.datatype.number({
+                        'min': 0,
+                        'max': results.length-1});
 
-                itemWeigth = parseInt(results[random]['WEIGHT [g]']);
-                let itemUsefulness = parseInt(results[random]['USEFULNESS [1-100]']);
+                    let iWeight = parseFloat(results[iRandom]['WEIGHT [g]']);
+                    let iUsefulness = parseInt(results[iRandom]['USEFULNESS [1-100]']);
 
-                totalWeight = totalWeight + itemWeigth;
-                if(totalWeight<maxWeight){
-                    arrHM.push(random);
-                    totalUsefulness= totalUsefulness + itemUsefulness;
-                } 
-            }
+                    // checking the weight of items
+                    totalWeight = totalWeight + iWeight;
+                    if (totalWeight <= maxWeight) {
+                        tmpHM.push(iRandom);
+                        totalUsefulness = totalUsefulness + iUsefulness;
+                    } 
+                }
 
-        totalWeight=totalWeight-itemWeigth;
-        // console.log('total weight: ' + totalWeight);
-            let arrFP=[];
-            arrFP.push(totalUsefulness);
+            HM.push(tmpHM);
+            FP.push(totalUsefulness);
+            // console.log("ostatni wynik: " + HM[i]);
+            // console.log("funkcja przystosowania [suma przydatności]: " + FP[i]);
+        }
 
-        FP.push(arrFP);
-        HM.push(arrHM);
-        console.log("ostatni wynik: " + HM[i]);
-        console.log("funkcja przystosowania [suma przydatności]: " + FP[i]);
-    }
-});
-
-fs.createReadStream(__dirname+'/data.csv').pipe(parser);
-
+        algNextStep();
+    }));
 }
+
 module.exports = algorytm;
